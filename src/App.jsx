545461d70643e38b2b508900bc7165f8654e6631
aksbrar <aksbrar34@gmail.com>
2025-09-03 10:13:15 -0400

    reusable button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import './App.css'
+import Button from './components/Button'
 
 function App() {
-
+  return (
+    <Button text="Reusable"/>
+  )
 }
 
 export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+export default function Button(props){
+  const style = {
+    background: "rgba(255, 255, 255, 0.2)",
+    borderRadius: "16px",
+    padding: "20px",
+    backdropFilter: "blur(10px)",
+    WebkitBackdropFilter: "blur(10px)",
+    border: "1px solid black",
+    boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+    color: "black"
+  }
+
+  return (
+    <button style={style} className="reusable-button">{props.text}</button>)
+}
