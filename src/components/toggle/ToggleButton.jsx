501ee495ec3eb36e5df039e6de8c78fce9e9c572
aksbrar <aksbrar34@gmail.com>
2025-09-06 13:08:52 -0400

    final refactor

--- src/components/toggle/ToggleButton.jsx
+++ src/components/toggle/ToggleButton.jsx
@@ -4,8 +4,7 @@ import { ToggleContext } from "./Toggle";
 
 export default function ToggleButton({children}){
   const {toggle} = React.useContext(ToggleContext)
-  console.log(toggle)
-
+  
   return (
     <div onClick={toggle}>{children}</div>
   )
