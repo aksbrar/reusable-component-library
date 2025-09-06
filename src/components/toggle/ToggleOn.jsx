c9668a73713c74ec13bb2a7e36be05f2a8d70ad0
aksbrar <aksbrar34@gmail.com>
2025-09-06 12:59:26 -0400

    toggle on to show when state is true

---
+++ src/components/toggle/ToggleOn.jsx
@@ -0,0 +1,9 @@
+// imports
+import React from "react";
+import { ToggleContext } from "./Toggle";
+
+export default function ToggleOn({children}){
+  const {on} = React.useContext(ToggleContext)
+
+  return on ? children : null
+}
