0fefb7bea9bfcf177f0f341116eabd0d9658192d
aksbrar <aksbrar34@gmail.com>
2025-09-09 22:27:46 -0400

    toggle colored box

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,16 @@ import Toggle from './components/toggle/index'
 
 function App() {
   return (
-    <>
-      <Toggle>
-        <Toggle.Button>
-          <Toggle.On>
-            <div className="box filled"></div>
-          </Toggle.On>
-          <Toggle.Off>
-            <div className="box"></div>
-          </Toggle.Off>
-        </Toggle.Button>
-      </Toggle>
-    </>  
+    <Toggle>
+      <Toggle.Button>
+        <Toggle.on>
+          <div className="box filled"></div>
+        </Toggle.on>
+        <Toggle.off>
+          <div className="box"></div>
+        </Toggle.off>
+      </Toggle.Button>
+    </Toggle>
   )
 }
 
