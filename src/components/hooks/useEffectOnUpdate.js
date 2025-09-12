a23db86622b869ebc6e918af6e64c75ea2c24d40
aksbrar <aksbrar34@gmail.com>
2025-09-12 12:23:50 -0400

    custom hook

---
+++ src/components/hooks/useEffectOnUpdate.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function useEffectOnUpdate(clbckfun, dep){
+  const firstRender = React.useRef(true)
+    React.useEffect(() => {
+        if (firstRender.current) {
+            firstRender.current = false
+        } else {
+            clbckfun
+        }
+    }, [dep])
+}
